9cf4ac91cf1d7dbaab3ce78cdf69a5bf000118b8
Carlos Faustino <chfcchfc96@gmail.com>
2024-05-20 15:43:58 -0300

    feat: setup tests

---
+++ test/setup-e2e.ts
@@ -0,0 +1,38 @@
+import "dotenv/config"
+
+import { execSync } from "node:child_process"
+import { randomUUID } from "node:crypto"
+
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
+
+function generateDbUrl(databaseName: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error("Please provide a DATABASE_URL environment variable")
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.pathname = `/${databaseName}`
+
+  return url.toString()
+}
+
+const schema = randomUUID()
+
+const dbName = `nest_clean_${schema.replace(/-/g, "_")}`
+
+beforeAll(async () => {
+  const dbUrl = generateDbUrl(dbName)
+
+  process.env.DATABASE_URL = dbUrl
+
+  execSync("pnpm prisma migrate deploy")
+})
+
+afterAll(async () => {
+  await prisma.$executeRawUnsafe(`DROP DATABASE IF EXISTS ${dbName};`)
+
+  await prisma.$disconnect()
+})
